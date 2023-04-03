89cd95893c87992fae2e04e7a3a59b8c659987dc
PJ <126437129+HILLSTV@users.noreply.github.com>
2023-04-03 14:49:08 -0400

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,1 +1,17 @@
-<script async src="//jsfiddle.net/fc9sru4v/2/embed/result/"><script>
+var grandmalvl = 1
+var cookie = 1
+function myFunction() {
+cookie += grandmalvl
+  document.getElementById("demo").innerHTML = cookie + " cookies clicked";
+}
+function myFunction1() {
+if (cookie > 30)
+grandmalvl += 1
+cookie -= 30
+document.getElementById("demo1").innerHTML = " ";
+  document.getElementById("demo").innerHTML = "Grandma Powerup level " + grandmalvl;
+  
+} 
+
+
+
